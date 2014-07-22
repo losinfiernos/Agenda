4617f74a8c0990cbac03b460771f66fa4e11df49
jortegui <jeortegac@gmail.com>
2014-07-22 18:35:14 +0200

    fichero de prueba clase terminal

---
+++ prueba.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from Terminal import Terminal
+#from Contacto import Contacto
+
+mi_terminal = Terminal()
+mis_opciones = ["saltar", "bailar", "correr"]
+mi_terminal.mostrar_menu(mis_opciones)
+
+
+
