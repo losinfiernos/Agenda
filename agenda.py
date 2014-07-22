f587e28baaf486fcf536f8c243e8b03cea3abc37
Enrique Crespo <jecrespom@gmail.com>
2014-07-22 18:02:53 +0200

    Principal de la agenda

---
+++ agenda.py
@@ -0,0 +1,12 @@
+'''
+Created on 22/07/2014
+
+@author: thinktic
+'''
+import Contacto
+import Interface
+import Datos
+
+interface = Interface("Terminal")
+datos = Datos("Fichero")
+interface.menu()
